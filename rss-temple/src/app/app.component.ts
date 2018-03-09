4b50553828e086f250334f8db70edff0ace1265b
Murray Christopherson <murraychristopherson@gmail.com>
2018-03-08 22:26:03 -0500

    Starting to add FB login

--- rss-temple/src/app/app.component.ts
+++ rss-temple/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { AuthService, SocialUser } from "angularx-social-login";
+import { FacebookLoginProvider } from "angularx-social-login";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  private user: SocialUser;
+  private loggedIn: boolean;
+
+  title = 'Tour of Heroes';
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+    });
+  }
+
+  signInWithFB(): void {
+    this.authService.signIn(FacebookLoginProvider.PROVIDER_ID).then(user => {
+      console.log(user);
+    });
+  }
 }
--- rss-temple/src/app/app.module.ts
+++ rss-temple/src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { SocialLoginModule, AuthServiceConfig, LoginOpt } from "angularx-social-login";
+import { FacebookLoginProvider } from "angularx-social-login";
+
 
 import { AppComponent } from './app.component';
 
+const fbLoginOptions: LoginOpt = {
+  scope: 'email'
+};
+
+let config = new AuthServiceConfig([
+  {
+    id: FacebookLoginProvider.PROVIDER_ID,
+    provider: new FacebookLoginProvider("Facebook-App-Id", fbLoginOptions)
+  }
+]);
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SocialLoginModule.initialize(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
