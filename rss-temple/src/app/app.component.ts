a960c31750d8d602a6264f3c64be561b5b2f15a4
Murray Christopherson <murraychristopherson@gmail.com>
2018-06-06 12:20:15 -0400

    Started minor experiments to get a login screen

--- rss-temple/src/app/app.component.ts
+++ rss-temple/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SessionService } from './services/session.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tour of Heroes';
+
+  username: string = '';
+  isLoggedIn: boolean = false;
+
+  constructor(private sessionService: SessionService) {
+
+  }
+
+  ngOnInit() {
+    this.isLoggedIn = this.sessionService.isLoggedIn();
+  }
+
+  logIn(username: string, password: string) {
+    alert(username + ' ' + password);
+  }
 }
--- rss-temple/src/app/app.module.ts
+++ rss-temple/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SessionService } from './services/session.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    SessionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
