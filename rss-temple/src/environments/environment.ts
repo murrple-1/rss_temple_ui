5f69a150b49bfb86680fcbf8a886b11ab4f5bfaf
Murray Christopherson <murraychristopherson@gmail.com>
2018-09-25 21:24:46 -0400

    Committing FB and Google public API keys

--- rss-temple/src/environments/environment.ts
+++ rss-temple/src/environments/environment.ts
@@ -5,8 +5,8 @@
 export const environment = {
   production: false,
   apiHost: 'http://localhost:8000',
-  googleApiClientId: '',
-  facebookAppId: '',
+  googleApiClientId: '273767134750-qvl91ooln1n75ic0g5ifqn3f6p6k9kfe.apps.googleusercontent.com',
+  facebookAppId: '206845406716212',
 };
 
 /*
