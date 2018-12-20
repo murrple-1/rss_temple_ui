103d1350f48c91ae680a807b562f578863f9fbf6
Murray Christopherson <murraychristopherson@gmail.com>
2018-12-19 22:16:06 -0500

    adding service to check OPML (and other) subscription progress

---
+++ rss-temple/src/app/_services/data/progress.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { sessionToken } from '@app/_modules/session.module';
+import {
+    CommonOptions,
+    toHeader as commonToHeader,
+} from '@app/_services/data/common.interface';
+
+import { environment } from '@environments/environment';
+
+@Injectable()
+export class ProgressService {
+    constructor(
+        private http: HttpClient,
+    ) { }
+
+    checkProgress(uuid: string, options: CommonOptions = {}) {
+        const headers = commonToHeader(options, sessionToken);
+
+        return this.http.get(`${environment.apiHost}/api/feed/subscribe/progress/${uuid}`, {
+            headers: headers,
+        });
+    }
+}
