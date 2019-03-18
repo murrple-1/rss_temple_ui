7fdd61acaee2d95c82b244dee1c927a8f5dde09e
Murray Christopherson <murraychristopherson@gmail.com>
2019-03-17 21:19:53 -0400

    a bunch of functionality for the verify page

--- rss-temple/src/app/verify/verify.component.ts
+++ rss-temple/src/app/verify/verify.component.ts
@@ -1,17 +1,64 @@
-﻿import { Component, OnInit, OnDestroy } from '@angular/core';
+﻿import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+import { UserService } from '@app/_services/data';
+import { HttpErrorService } from '@app/_services';
+
+enum State {
+  NotStarted,
+  Sending,
+  Success,
+  Error,
+  NoToken,
+}
 
 @Component({
   templateUrl: 'verify.component.html',
   styleUrls: ['verify.component.scss'],
 })
 export class VerifyComponent implements OnInit, OnDestroy {
+  readonly State = State;
+  state = State.NotStarted;
+
   private unsubscribe$ = new Subject<void>();
 
-  constructor() {}
+  constructor(
+    private zone: NgZone,
+    private activatedRoute: ActivatedRoute,
+    private userService: UserService,
+    private httpErrorService: HttpErrorService,
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const token = this.activatedRoute.snapshot.queryParamMap.get('token');
+
+    if (token === null) {
+      this.state = State.NoToken;
+    } else {
+      this.state = State.Sending;
+
+      this.userService
+        .verify(token)
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe({
+          next: () => {
+            this.zone.run(() => {
+              this.state = State.Success;
+            });
+          },
+          error: error => {
+            this.httpErrorService.handleError(error);
+
+            this.zone.run(() => {
+              this.state = State.Error;
+            });
+          },
+        });
+    }
+  }
 
   ngOnDestroy() {
     this.unsubscribe$.next();
