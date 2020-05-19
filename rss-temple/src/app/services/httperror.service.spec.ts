cfd60723c9e3f1166d0da39adec8a935418d41f2
Murray Christopherson <murraychristopherson@gmail.com>
2020-05-18 22:13:15 -0400

    more tests

--- rss-temple/src/app/services/httperror.service.spec.ts
+++ rss-temple/src/app/services/httperror.service.spec.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { AlertService } from '@app/services/alert.service';
 
 import { HttpErrorService } from './httperror.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 function setup() {
   const routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
@@ -20,12 +21,56 @@ function setup() {
 }
 
 describe('HttpErrorService', () => {
-  it('should handle errors', () => {
-    const { httpErrorService } = setup();
+  it('should handle HttpErrorResponses', () => {
+    const { httpErrorService, alertServiceSpy } = setup();
+    const response = new HttpErrorResponse({
+      status: 400,
+    });
 
-    httpErrorService.handleError('something');
-    expect().nothing();
+    httpErrorService.handleError(response);
+    expect(alertServiceSpy.error).toHaveBeenCalledWith(
+      'Unknown Error',
+      jasmine.any(Number),
+    );
   });
 
-  // TODO more tests
+  it('should handle "no response" HttpErrorResponses', () => {
+    const { httpErrorService, alertServiceSpy } = setup();
+    const response = new HttpErrorResponse({
+      status: 0,
+    });
+
+    httpErrorService.handleError(response);
+    expect(alertServiceSpy.error).toHaveBeenCalledWith(
+      'Unable to connect to server',
+      jasmine.any(Number),
+    );
+  });
+
+  it('should handle "session expired" HttpErrorResponses', () => {
+    const { httpErrorService, alertServiceSpy, routerSpy } = setup();
+    const response = new HttpErrorResponse({
+      status: 401,
+    });
+
+    httpErrorService.handleError(response);
+    expect(alertServiceSpy.error).toHaveBeenCalledWith(
+      'Session expired',
+      jasmine.any(Number),
+    );
+    expect(routerSpy.navigate).toHaveBeenCalledWith(
+      ['/login'],
+      jasmine.any(Object),
+    );
+  });
+
+  it('should handle arbitrary errors', () => {
+    const { httpErrorService, alertServiceSpy } = setup();
+
+    httpErrorService.handleError('something');
+    expect(alertServiceSpy.error).toHaveBeenCalledWith(
+      'Unknown Error',
+      jasmine.any(Number),
+    );
+  });
 });
