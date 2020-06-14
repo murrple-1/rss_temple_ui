6f25affebe07f5575d94226a3971f1ce3d239203
Murray Christopherson <murraychristopherson@gmail.com>
2020-06-14 17:39:56 -0400

    more tests

---
+++ rss-temple/src/app/services/data/opml.service.spec.ts
@@ -0,0 +1,65 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { fakeAsync } from '@angular/core/testing';
+
+import { of } from 'rxjs';
+
+import { OPMLService } from './opml.service';
+
+function setup() {
+  const httpClientSpy = jasmine.createSpyObj<HttpClient>('HttpClient', [
+    'get',
+    'post',
+  ]);
+
+  const opmlService = new OPMLService(httpClientSpy);
+
+  return {
+    httpClientSpy,
+
+    opmlService,
+  };
+}
+
+describe('opml.service', () => {
+  it('should download', fakeAsync(async () => {
+    const { httpClientSpy, opmlService } = setup();
+
+    const downloadText = '<opml></opml>'; // not real OPML
+
+    httpClientSpy.get.and.returnValue(of(downloadText));
+
+    const xmlText = await opmlService.download().toPromise();
+
+    expect(xmlText).toBe(downloadText);
+  }));
+
+  it('should upload text', fakeAsync(async () => {
+    const { httpClientSpy, opmlService } = setup();
+
+    const response = new HttpResponse({
+      status: 200,
+    });
+
+    httpClientSpy.post.and.returnValue(of(response));
+
+    const response_ = await opmlService.upload('<opml></opml>').toPromise();
+
+    expect(response_.status).toBe(200);
+  }));
+
+  it('should upload buffer', fakeAsync(async () => {
+    const { httpClientSpy, opmlService } = setup();
+
+    const response = new HttpResponse({
+      status: 200,
+    });
+
+    httpClientSpy.post.and.returnValue(of(response));
+
+    const response_ = await opmlService
+      .upload(new ArrayBuffer(100))
+      .toPromise();
+
+    expect(response_.status).toBe(200);
+  }));
+});
