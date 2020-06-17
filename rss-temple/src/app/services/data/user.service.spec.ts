45330f4aa43208a93f202803258970b5add10e4c
Murray Christopherson <murraychristopherson@gmail.com>
2020-06-16 22:41:13 -0400

    test stubs

---
+++ rss-temple/src/app/services/data/user.service.spec.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { fakeAsync } from '@angular/core/testing';
+
+import { of } from 'rxjs';
+
+import { UserService } from './user.service';
+
+function setup() {
+  const httpClientSpy = jasmine.createSpyObj<HttpClient>('HttpClient', ['get']);
+
+  const userService = new UserService(httpClientSpy);
+
+  return {
+    httpClientSpy,
+
+    userService,
+  };
+}
+
+describe('user.service', () => {
+  it('should', fakeAsync(async () => {
+    const { httpClientSpy, userService } = setup();
+    expect().nothing();
+  }));
+});
---
+++ rss-temple/src/app/services/data/usercategory.service.spec.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { fakeAsync } from '@angular/core/testing';
+
+import { of } from 'rxjs';
+
+import { UserCategoryService } from './usercategory.service';
+
+function setup() {
+  const httpClientSpy = jasmine.createSpyObj<HttpClient>('HttpClient', ['get']);
+
+  const userCategoryService = new UserCategoryService(httpClientSpy);
+
+  return {
+    httpClientSpy,
+
+    userCategoryService,
+  };
+}
+
+describe('usercategory.service', () => {
+  it('should', fakeAsync(async () => {
+    const { httpClientSpy, userCategoryService } = setup();
+    expect().nothing();
+  }));
+});
