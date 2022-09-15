6b48b2e8005649a286d98247d9e7a3bd53f22217
Murray Christopherson <murraychristopherson@gmail.com>
2022-09-14 21:09:49 -0700

    environment files are moving around a bit

--- rss-temple/src/environments/environment.dev.ts
+++ rss-temple/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 import { IEnvironment } from '@environments/ienvironment';
 
 export const environment: IEnvironment = {
-  production: true,
+  production: false,
   apiHost: 'https://api.rsstemple.com',
   googleApiClientId: '',
   facebookAppId: '',
