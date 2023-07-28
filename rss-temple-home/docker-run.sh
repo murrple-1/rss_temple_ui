6f7f93c0cb2b74daed252d7d028c3ccc01753769
Murray Christopherson <murraychristopherson@gmail.com>
2023-07-27 22:51:03 -0700

    changing the Dockerfiles to reflect the changes

--- rss-temple-home/docker-run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env sh
-
-set -eux
-
-yarn run grunt build --app-url="$APP_URL" --twitter-url="$TWITTER_URL" --fb-url="$FB_URL" --insta-url="$INSTA_URL"
-
-exec caddy run --config ./Caddyfile --adapter "caddyfile"
--- rss-temple/docker-run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-set -eux
-
-yarn generate-licenses-file
-yarn collect-envvars
-yarn run ng build -c ${1:-production}
-
-exec caddy run --config ./Caddyfile --adapter "caddyfile"
