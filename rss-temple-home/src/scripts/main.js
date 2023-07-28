2417d70c90bf863d846fd4aa82528fecccd88c80
Murray Christopherson <murraychristopherson@gmail.com>
2023-07-27 22:50:43 -0700

    switching from environment based to file based configuration for home too

--- rss-temple-home/src/scripts/main.js
+++ rss-temple-home/src/scripts/main.js
@@ -1,3 +1,13 @@
-import("jquery/src/jquery").then(() => {
+import("jquery/src/jquery").then((jquery) => {
   import("bootstrap/dist/js/bootstrap.bundle");
+
+  const $ = jquery.default;
+  fetch("/config.json")
+    .then((r) => r.json())
+    .then((json_) => {
+      $("a#app-anchor").attr("href", json_.appUrl);
+      $("a#facebook-anchor").attr("href", json_.facebookUrl);
+      $("a#twitter-anchor").attr("href", json_.twitterUrl);
+      $("a#instagram-anchor").attr("href", json_.instagramUrl);
+    });
 });
