96ebde74a7832d78800994229ed89eb2eefa6146
Murray Christopherson <murraychristopherson@gmail.com>
2023-07-28 20:41:39 -0700

    keep home running, even when it's done, so it doesn't get GC'd

--- rss-temple-home/docker-run.sh
+++ rss-temple-home/docker-run.sh
@@ -7,3 +7,7 @@ set -eux
 yarn run grunt build --app-url="$APP_URL" --twitter-url="$TWITTER_URL" --fb-url="$FB_URL" --insta-url="$INSTA_URL"
 
 rm -rf /srv/* && cp -R /build/dist/* /srv/
+
+while :
+  sleep 60
+done
