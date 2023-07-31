d64455b63b6881c423065dbdbfcfd676f2271895
Murray Christopherson <murraychristopherson@gmail.com>
2023-07-30 18:35:37 -0700

    fixing tests

--- rss-temple/src/app/test/activatedroute.mock.ts
+++ rss-temple/src/app/test/activatedroute.mock.ts
@@ -9,7 +9,7 @@ import {
 } from '@angular/router';
 import { Type } from '@angular/core';
 
-import { Observable, Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 class MockParamMap implements ParamMap {
   _map = new Map<string, string>();
@@ -36,92 +36,30 @@ class MockParamMap implements ParamMap {
   }
 }
 
-class MockActivatedRouteSnapshot implements ActivatedRouteSnapshot {
+class MockActivatedRouteSnapshot extends ActivatedRouteSnapshot {
   url: UrlSegment[] = [];
   params: Params = {};
   queryParams: Params = {};
   fragment = '';
   data: Data = {};
   outlet = '';
-  component: string | Type<any> | null = null;
+  component: Type<any> | null = null;
   routeConfig: Route | null = null;
 
   _paramMap = new MockParamMap();
 
-  get root(): ActivatedRouteSnapshot {
-    throw new Error('Method not implemented.');
-  }
-
-  get parent(): ActivatedRouteSnapshot | null {
-    throw new Error('Method not implemented.');
-  }
-
-  get firstChild(): ActivatedRouteSnapshot | null {
-    throw new Error('Method not implemented.');
-  }
-
-  get children(): ActivatedRouteSnapshot[] {
-    throw new Error('Method not implemented.');
-  }
-
-  get pathFromRoot(): ActivatedRouteSnapshot[] {
-    throw new Error('Method not implemented.');
-  }
-
   get paramMap(): ParamMap {
     return this._paramMap;
   }
-
-  get queryParamMap(): ParamMap {
-    throw new Error('Method not implemented.');
-  }
-
-  toString(): string {
-    throw new Error('Method not implemented.');
-  }
 }
 
-export class MockActivatedRoute implements ActivatedRoute {
+export class MockActivatedRoute extends ActivatedRoute {
   url = new Observable<UrlSegment[]>(_subscriber => {});
   params = new Observable<Params>(_subscriber => {});
   queryParams = new Observable<Params>(_subscriber => {});
   fragment = new Observable<string>(_subscriber => {});
   data = new Observable<Data>(_subscriber => {});
   outlet = '';
-  component: string | Type<any> | null = null;
+  component: Type<any> | null = null;
   snapshot = new MockActivatedRouteSnapshot();
-
-  get routeConfig(): Route | null {
-    throw new Error('Method not implemented.');
-  }
-
-  get root(): ActivatedRoute {
-    throw new Error('Method not implemented.');
-  }
-
-  get parent(): ActivatedRoute | null {
-    throw new Error('Method not implemented.');
-  }
-
-  get firstChild(): ActivatedRoute | null {
-    throw new Error('Method not implemented.');
-  }
-
-  get children(): ActivatedRoute[] {
-    throw new Error('Method not implemented.');
-  }
-
-  get pathFromRoot(): ActivatedRoute[] {
-    throw new Error('Method not implemented.');
-  }
-
-  readonly paramMap = new Subject<ParamMap>();
-
-  get queryParamMap(): Observable<ParamMap> {
-    throw new Error('Method not implemented.');
-  }
-
-  toString(): string {
-    throw new Error('Method not implemented.');
-  }
 }
