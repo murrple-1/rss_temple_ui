4e9899b7df3a3092acf08ee5fa4c9b8c38594295
Murray Christopherson <murraychristopherson@gmail.com>
2023-09-18 19:59:41 -0700

    better login bg image

--- rss-temple/tools/login_image_gen/login_image_gen.py
+++ rss-temple/tools/login_image_gen/login_image_gen.py
@@ -4,82 +4,25 @@ import random
 def main():
     svg = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">'
 
-    fills = ["#f3b700", "#faa300", "#e57c04", "#ff6201", "#f63e02"]
-    coords = [i for i in range(0, 1025, 256)]
-    for i in range(50):
-        fill = fills[i % len(fills)]
-        x = random.choice(coords)
-        y = random.choice(coords)
-        step = random.choice([128, 256, 512])
+    fills: dict[str | None, int] = {
+        None: 1,
+        "#F6953788": 3,
+        "#EF7B0B88": 3,
+        "#3798F688": 3,
+        "#00567A88": 3,
+    }
+    for start_x in range(0, 1025, 64):
+        for start_y in range(0, 1025, 64 * 2):
+            fill = random.choices(list(fills.keys()), list(fills.values()))[0]
 
-        available_mod_fns = []
-        if (x + step) <= 1024:
-            if (y + step) <= 1024:
-                available_mod_fns.extend(
-                    [
-                        [
-                            lambda x, y, step: (x, y + step),
-                            lambda x, y, step: (x + step, y),
-                        ],
-                        [
-                            lambda x, y, step: (x + step, y),
-                            lambda x, y, step: (x, y + step),
-                        ],
-                    ]
-                )
-            if (y - step) >= 0:
-                available_mod_fns.extend(
-                    [
-                        [
-                            lambda x, y, step: (x + step, y),
-                            lambda x, y, step: (x, y - step),
-                        ],
-                        [
-                            lambda x, y, step: (x, y - step),
-                            lambda x, y, step: (x + step, y),
-                        ],
-                    ]
-                )
-        if (x - step) >= 0:
-            if (y + step) <= 1024:
-                available_mod_fns.extend(
-                    [
-                        [
-                            lambda x, y, step: (x, y + step),
-                            lambda x, y, step: (x - step, y),
-                        ],
-                        [
-                            lambda x, y, step: (x - step, y),
-                            lambda x, y, step: (x, y + step),
-                        ],
-                    ]
-                )
-            if (y - step) >= 0:
-                available_mod_fns.extend(
-                    [
-                        [
-                            lambda x, y, step: (x, y - step),
-                            lambda x, y, step: (x - step, y),
-                        ],
-                        [
-                            lambda x, y, step: (x - step, y),
-                            lambda x, y, step: (x, y - step),
-                        ],
-                    ]
-                )
+            if fill is not None:
+                svg += f'<polygon fill="{fill}" points="{start_x} {start_y}, {start_x + 64} {start_y + 64}, {start_x} {start_y + 128}" />'
 
-        try:
-            mod_fns = random.choice(available_mod_fns)
-        except Exception:
-            print(f"{x=} {y=}, {step=}")
-            raise
-        svg += f'<polygon fill="{fill}" points="'
-        for mod_fn in [lambda x, y, step: (x, y)] + mod_fns:
-            x, y = mod_fn(x, y, step)
-            svg += f"{x} {y}, "
+        for start_y in range(-64, 1025, 64 * 2):
+            fill = random.choices(list(fills.keys()), list(fills.values()))[0]
 
-        svg = svg[:-2]
-        svg += '" />'
+            if fill is not None:
+                svg += f'<polygon fill="{fill}" points="{start_x} {start_y}, {start_x - 64} {start_y + 64}, {start_x} {start_y + 128}" />'
 
     svg += "</svg>"
     print(svg)
