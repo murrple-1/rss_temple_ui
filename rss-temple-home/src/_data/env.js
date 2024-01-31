c2f5166be3cb63d556383df5d1e535903b36670c
Murray Christopherson <murraychristopherson@gmail.com>
2024-01-31 12:11:46 -0800

    missed a couple

--- rss-temple-home/src/_data/env.js
+++ rss-temple-home/src/_data/env.js
@@ -1,5 +1,5 @@
 module.exports = {
-  app_url: process.env.APP_URL,
+  web_app_url: process.env.WEB_APP_URL,
   fb_url: process.env.FB_URL,
   twitter_url: process.env.TWITTER_URL,
   insta_url: process.env.INSTA_URL
