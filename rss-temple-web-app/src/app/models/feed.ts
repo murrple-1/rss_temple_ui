b995cab9a40238d68501ada0ffc66615809a58e6
Murray Christopherson <murraychristopherson@gmail.com>
2024-03-19 14:41:33 -0700

    adding new field - not yet used

--- rss-temple-web-app/src/app/models/feed.ts
+++ rss-temple-web-app/src/app/models/feed.ts
@@ -19,6 +19,7 @@ export const ZFeed = z
     userCategoryUuids: z.array(z.string().uuid()),
     unreadCount: z.number(),
     readCount: z.number(),
+    isDormant: z.boolean(),
   })
   .partial();
 
--- rss-temple-web-app/src/app/services/data/feed.service.ts
+++ rss-temple-web-app/src/app/services/data/feed.service.ts
@@ -28,7 +28,7 @@ import {
 import { queryAllFn } from '@app/services/data/queryall.function';
 
 export type Field = keyof Feed;
-export type SortField = keyof Feed;
+export type SortField = keyof Omit<Feed, 'isDormant'>;
 
 const ZExposedFeed = z.object({
   title: z.string(),
