8339e66ae55d8b0b3f7b7f601c2f157c18d3f7db
Murray Christopherson <murraychristopherson@gmail.com>
2024-04-12 18:14:15 -0700

    adding privacy policy link to cookie consent popover

--- rss-temple-web-app/src/app/components/cookie-consent-snackbar/cookie-consent-snackbar.component.ts
+++ rss-temple-web-app/src/app/components/cookie-consent-snackbar/cookie-consent-snackbar.component.ts
@@ -1,13 +1,23 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { ConfigService } from '@app/services';
+
 @Component({
   selector: 'app-cookie-consent-snackbar',
   templateUrl: './cookie-consent-snackbar.component.html',
   styleUrls: ['./cookie-consent-snackbar.component.scss'],
 })
 export class CookieConsentSnackbarComponent {
+  privacyPolicyUrl: string | null;
+
   @Output()
   cookieConsentAcknowledged = new EventEmitter<void>();
+
+  constructor(configService: ConfigService) {
+    const privacyPolicyUrl = configService.get('privacyPolicyUrl');
+    this.privacyPolicyUrl =
+      typeof privacyPolicyUrl === 'string' ? privacyPolicyUrl : null;
+  }
 
   onAcknowledge() {
     this.cookieConsentAcknowledged.emit();
