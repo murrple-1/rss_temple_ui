3787a3fb6c0d60b7c8d81f578102c5b72df85415
Murray Christopherson <murraychristopherson@gmail.com>
2024-04-14 20:35:20 -0700

    new contact page example

--- rss-temple-home/src/contact.11tydata.js
+++ rss-temple-home/src/contact.11tydata.js
@@ -1,7 +1,20 @@
+const fs = require("fs");
+const f = fs.openSync("default_content/contact.html", "r");
+const defaultContent = fs.readFileSync(f, "utf8");
+fs.closeSync(f);
+
 module.exports = function () {
   return {
     layout: "standard.liquid",
     page_name: "Contact",
-    head_custom_filename: "contact.head.html"
+    head_custom_filename: "contact.head.html",
+    default_content: defaultContent,
+    additional_links: [
+      {
+        href: "/styles/legal.min.css",
+        rel: "stylesheet",
+        type: "text/css"
+      }
+    ]
   };
 };
