6448a8c023060c7ba4b95eb3349f0d7c9d5a63fe
Murray Christopherson <murraychristopherson@gmail.com>
2024-05-23 09:51:51 -0700

    fix home JS

--- rss-temple-home/src/scripts/main.js
+++ rss-temple-home/src/scripts/main.js
@@ -1,3 +1,7 @@
-import("jquery/src/jquery").then(() => {
-  import("bootstrap/dist/js/bootstrap.bundle");
+const document = window.document;
+
+document.addEventListener("DOMContentLoaded", () => {
+  import("jquery/dist/jquery").then(() => {
+    import("bootstrap/dist/js/bootstrap.bundle");
+  });
 });
