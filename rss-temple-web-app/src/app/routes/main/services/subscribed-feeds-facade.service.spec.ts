533727b61911593fcaf5716ef55794250612db98
Murray Christopherson <murraychristopherson@gmail.com>
2024-07-20 14:32:32 -0700

    fix tests

--- rss-temple-web-app/src/app/routes/main/services/subscribed-feeds-facade.service.spec.ts
+++ rss-temple-web-app/src/app/routes/main/services/subscribed-feeds-facade.service.spec.ts
@@ -1,13 +1,26 @@
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
+
 import { FeedService } from '@app/services/data';
 
 import { SubscribedFeedsFacadeService } from './subscribed-feeds-facade.service';
 
 function setup() {
+  const mockRouter = jasmine.createSpyObj<Router>(
+    'Router',
+    {},
+    {
+      'events': of(),
+    },
+  );
   const mockFeedService = jasmine.createSpyObj<FeedService>('FeedService', [
     'query',
   ]);
 
-  const feedsFacadeService = new SubscribedFeedsFacadeService(mockFeedService);
+  const feedsFacadeService = new SubscribedFeedsFacadeService(
+    mockRouter,
+    mockFeedService,
+  );
 
   return {
     feedsFacadeService,
