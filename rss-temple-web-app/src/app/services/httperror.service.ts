c5ea3a25ad12e5fc83cd21eb44cefc72a89f9105
Murray Christopherson <murraychristopherson@gmail.com>
2024-07-20 17:36:29 -0700

    refreshing should no longer cause as many app alerts via cancelled requests

--- rss-temple-web-app/src/app/services/httperror.service.ts
+++ rss-temple-web-app/src/app/services/httperror.service.ts
@@ -9,11 +9,24 @@ import { AuthStateService } from '@app/services/auth-state.service';
   providedIn: 'root',
 })
 export class HttpErrorService {
+  private static WINDOWS_EVENT_LISTENER: (() => void) | null = null;
+  private static APP_ALERTS_ENABLED = true;
+
   constructor(
     private router: Router,
     private appAlertsService: AppAlertsService,
     private authStateService: AuthStateService,
-  ) {}
+  ) {
+    if (HttpErrorService.WINDOWS_EVENT_LISTENER === null) {
+      HttpErrorService.WINDOWS_EVENT_LISTENER = () => {
+        HttpErrorService.APP_ALERTS_ENABLED = false;
+      };
+      window.addEventListener(
+        'beforeunload',
+        HttpErrorService.WINDOWS_EVENT_LISTENER,
+      );
+    }
+  }
 
   handleError(error: unknown) {
     let errorMessage = 'Unknown Error';
@@ -40,11 +53,13 @@ export class HttpErrorService {
 
     console.error(errorMessage, error);
 
-    this.appAlertsService.appAlertDescriptor$.next({
-      autoCloseInterval: 5000,
-      canClose: true,
-      text: errorMessage,
-      type: 'danger',
-    });
+    if (HttpErrorService.APP_ALERTS_ENABLED) {
+      this.appAlertsService.appAlertDescriptor$.next({
+        autoCloseInterval: 5000,
+        canClose: true,
+        text: errorMessage,
+        type: 'danger',
+      });
+    }
   }
 }
